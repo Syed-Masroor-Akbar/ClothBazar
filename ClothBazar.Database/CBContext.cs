879251e0790c11986d4ca42e690bfabac21c5666
fahim akbar <fahimcool123@gmail.com>
2023-06-27 15:07:35 +0530

    Add project files.

---
+++ ClothBazar.Database/CBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClothBazar.Entities;
+
+namespace ClothBazar.Database
+{
+    public class CBContext : DbContext
+    {
+        public CBContext() : base("ClothBazarConnection")
+        {
+
+        }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
